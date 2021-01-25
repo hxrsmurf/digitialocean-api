d0116f11f6cb6534a48dd63800e6eb3bbd1cb80e
Kevin Murphy <kevin@hxrsmurf.info>
2021-01-24 21:20:00 -0600

    Update myIP function for create, update, and delete

--- digitalocean.ps1
+++ digitalocean.ps1
@@ -9,12 +9,9 @@ foreach ($c in $config){
 		"api_token" { $apiToken = $c.value }
 		"key" { $sshKey = $c.value }
 		"home" { $homeName = $c.value }
+		"apiBase" { $apiBase = $c.value }
 	}
 }
-
-$apiToken = $apiToken
-$sshKey = $sshKey
-$apiBase = "https://api.digitalocean.com/v2/"
 
 $bearerToken = "Bearer " + $apiToken
 
@@ -86,22 +83,6 @@ function getDroplet {
 	return $output
 }
 
-function myIP {
-	$ipifyURL = "https://api.ipify.org"
-	$request = (Invoke-WebRequest -URI $ipifyURL).content
-	
-	# updateDomainRecord domain type name value recordid
-	# createDomainRecord domain type name value
-	
-	$domains = listDomains
-	
-	foreach ($domain in $domains.domain){
-		createDomainRecord $domain.name "A" "$homeName" $request
-	}
-	
-	return $request
-}
-
 function listDomains {
 	$apiURL = $apiBase + "domains"
 	$domains = ((Invoke-WebRequest -URI $apiURL -Headers $headers).content | ConvertFrom-JSON).domains
@@ -117,7 +98,7 @@ function listDomains {
 	return $output
 }
 
-function listDomainRecords ($domain) {
+function listDomainRecords ($domain, $export) {
 	if ($domain.length -eq 0){
 		Write-Host "No input"
 		return
@@ -150,16 +131,20 @@ function listDomainRecords ($domain) {
 		New-Item -Path $folderPath -ItemType "directory"
 	}
 	
-	$fileName = $folderPath + $domain + "-records-" + $fileDate + ".csv"
-	Write-Host $fileName
-	$output | Export-CSV -Delimiter "|" -NoTypeInformation -Path $fileName
+	if ($export -eq $true){
+		$fileName = $folderPath + $domain + "-records-" + $fileDate + ".csv"
+		Write-Host $fileName
+		$output | Export-CSV -Delimiter "|" -NoTypeInformation -Path $fileName
+		return $output
+	}
+	
 	return $output
 }
 
 function getDomainRecords {
 	$domains = listDomains
 	foreach ($domain in $domains.domain){
-		listDomainRecords $domain.name
+		listDomainRecords $domain.name $true
 	}
 }
 
@@ -182,6 +167,7 @@ function createDomainRecord {
 		$value
 	)
 	
+	# How to Use
 	# createDomainRecord domain type name value
 	
 	if ($domain.length -eq 0){
@@ -200,8 +186,6 @@ function createDomainRecord {
 	$request = Invoke-WebRequest -URI $apiURL -Headers $headers -body $body -Method POST
 	
 	if ($request.StatusCode -eq 201) {Write-Host "Created!"}
-	
-	return $request
 }
 
 function updateDomainRecord {
@@ -227,9 +211,12 @@ function updateDomainRecord {
 		$recordID
 	)
 	
-	# Backup Records first
-	listDomainRecords $domain
+	if ($domain -eq 0 -or $domain -eq $null){
+		Write-Host "Input data"
+		return
+	}
 	
+	# How to Use
 	# updateDomainRecord domain type name value recordid
 	
 	$body = @{
@@ -243,10 +230,8 @@ function updateDomainRecord {
 	
 	$body = @{
 		name = "$name"
-	} | ConvertTo-JSON
-	
-	Write-Host $apiURL
-	
+	} | ConvertTo-JSON	
+		
 	$request = Invoke-WebRequest -URI $apiURL -Headers $headers -body $body -Method PUT
 	return $request
 }
@@ -262,14 +247,17 @@ function deleteDomainRecord {
 		$recordID
 	)
 	
+	# How to Use
+	# deleteDomainRecord $domain $recordID
+	
 	# Backup Records first
-	listDomainRecords $domain
+	# listDomainRecords $domain $true
 
 	$apiURL = $apiBase + "domains/" + $domain + "/records/" + $recordID
 	
 	$request = Invoke-WebRequest -URI $apiURL -Headers $headers -Method DELETE
 	
-	if ($request.StatusCode -eq 204) {Write-Host "Deleted $recordID!"} else { "Not complete." }
+	if ($request.StatusCode -eq 204) {Write-Host "Deleted $recordID!"} else { "Not complete for $recordID"; return }
 }
 
 function bulkDeleteDomainRecords {
@@ -284,7 +272,7 @@ function bulkDeleteDomainRecords {
 	)
 	
 	# Backup Records first
-	listDomainRecords $domain
+	listDomainRecords $domain $true
 	
 	foreach ($recordID in $recordIDs){
 		Write-Host $recordID
@@ -319,4 +307,84 @@ function deleteDroplet ($dropletID) {
 	$request = Invoke-WebRequest -URI $apiURL -Method DELETE -headers $headers
 	if ($request.StatusCode -eq 204) {Write-Host "Success!"} else { "Not Successful." }
 	return $request
+}
+
+function myIP ($update, $action) {
+	$ipifyURL = "https://api.ipify.org"
+	$myIP = (Invoke-WebRequest -URI $ipifyURL).content
+	
+	if ($update -eq $true){
+		Write-Host "Updating..."
+		
+		$domains = listDomains
+			
+		# Parse domain's records
+		$output = @()
+		$domain = $null
+		
+		foreach ($domain in $domains.domain){
+			Write-Host "Checking" $domain.name
+			$domainRecords = listDomainRecords $domain.name $false
+			if ($domainRecords -eq $null) { Write-Host "No Records"; return }
+			foreach ($domainRecord in $domainRecords){
+				if ($domainRecord.data -eq $myIP -or $domainRecord.name -like $homeName) {
+					$drObject = New-Object -TypeName PSObject
+					$drInfo = [ordered]@{
+						recordID = $domainRecord.id
+						name = $domainRecord.name
+						domain = $domain.name
+						data = $domainRecord.data
+					}
+				
+					foreach($key in $drInfo.keys) {
+						$drObject | Add-Member -MemberType NoteProperty -Name $key -Value $drInfo[$key]
+					}
+					
+					$output += $drObject
+				}
+			}
+		}
+		
+		if ($output) {	
+			foreach ($out in $output){
+				$updateDomain = $out.domain
+				$updateName = $out.name
+				$updateRecordID = $out.recordID
+				$updateValue = $myIP
+				
+				$message = "$updateDomain $updateName $updateValue $updateRecordID"
+				
+				Write-Host "Checking $message"
+				
+				switch ($action){
+					"delete" {
+						if ($out.name -notlike "@"){
+							Write-Host "Deleting $message"
+							deleteDomainRecord $updateDomain $updateRecordID
+						}					
+					}
+					
+					# This doesn't work to update the IP...
+					"update" {
+						Write-Host "Updating $message"
+						# updateDomainRecord domain type name value recordid
+						updateDomainRecord $updateDomain "A" $updateName $updateValue $updateRecordID
+					}
+					
+					# Need to check if homeName exists and update that some how. Maybe a delete and add?
+					# Create MX records, too?
+					"create" {
+						Write-Host "Creating $message"
+						# createDomainRecord domain type name value
+						createDomainRecord $updateDomain "A" $homeName $updateValue
+						break
+					}
+					
+					default { return $myIP }
+				}
+			}
+		}
+	}
+	
+	return $myIP
 }
